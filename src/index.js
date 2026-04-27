9df73d8466c5065fe94e8cf08ecce88a83513151
nnoliveirah <nunesoliveiranathalia@gmail.com>
2026-04-27 23:04:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,12 +14,8 @@ function generatePoem(event) {
   let apiKey = "oe8c960c16447fea3112bt8f4b552230";
   let prompt = `Generate a short (Brazilian) Portuguese poem about ${instructionsInput.value}`;
   let context =
-    "Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem";
+    "Your mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  console.log("Generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
 
   axios.get(apiURL).then(displayPoem);
 }
