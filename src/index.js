044b4c4eb2b248366770cf7c9dbea8b7759ca1e4
nnoliveirah <nunesoliveiranathalia@gmail.com>
2026-04-27 23:53:42 +0200

    Final commit

--- src/index.js
+++ src/index.js
@@ -5,18 +5,41 @@ function displayPoem(response) {
     delay: 1,
     cursor: "",
   });
+
+  let submitButton = document.querySelector("#submit-button");
+  submitButton.removeAttribute("disabled");
+}
+
+function displaySheCodesPoem() {
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+
+  let poem = `Na folha em branco, a magia se faz,<br />
+Palavras dançam, no ritmo da paz,<br />
+Cada verso um mundo, um sonho profundo,<br />
+Poemas são vidas, ecos do nosso fundo.<br />
+<strong>SheCodes AI</strong>`;
+
+  new Typewriter("#poem", {
+    strings: poem,
+    autoStart: true,
+    delay: 1,
+  });
 }
 
 function generatePoem(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
+  let submitButton = document.querySelector(".submit-button");
+
+  submitButton.setAttribute("disabled", "disabled");
+
   let apiKey = "oe8c960c16447fea3112bt8f4b552230";
   let prompt = `Generate a short (Brazilian) Portuguese poem about ${instructionsInput.value}`;
   let context =
     "Your mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
   poemElement.innerHTML = `<div class="generating">⏳ Generating a poem in Portuguese about ${instructionsInput.value}</div>`;
@@ -26,3 +49,4 @@ function generatePoem(event) {
 
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
+displaySheCodesPoem();
