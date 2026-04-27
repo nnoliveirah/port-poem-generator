d261ee8162e8ad297217600ff4e563000169dd3f
nnoliveirah <nunesoliveiranathalia@gmail.com>
2026-04-27 22:42:23 +0200

    api commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings:
-      "No meio do caminho tinha uma pedra. Tinha uma pedra no meio do caminho",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "oe8c960c16447fea3112bt8f4b552230";
+  apiURL =
+    "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}";
+  let prompt = `Generate a short (Brazilian) Portuguese poem about ${instructionsInput.value}`;
+  let context =
+    "You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem";
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
