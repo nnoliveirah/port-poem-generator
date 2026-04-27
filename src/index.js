22e6dea310afa5cbbe91abd56c2aa4d775720f4d
nnoliveirah <nunesoliveiranathalia@gmail.com>
2026-04-27 23:02:42 +0200

    Fixing Bugs commit

--- src/index.js
+++ src/index.js
@@ -12,11 +12,10 @@ function generatePoem(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "oe8c960c16447fea3112bt8f4b552230";
-  apiURL =
-    "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}";
   let prompt = `Generate a short (Brazilian) Portuguese poem about ${instructionsInput.value}`;
   let context =
-    "You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem";
+    "Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
